595cbdff8a75c7fde7f510dfb162769abdd17929
Rodrigo Piccardo <rodpiccardo@gmail.com>
2025-11-02 20:33:42 -0500

    update

---
+++ week-2/project/index.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Rodrigo Piccardo
+ * Date: 11/1/2025
+ * File Name: index.js
+ * Description:
+*/
+
+// TODO: Import your module using require
+const { createRecipe, setTimer, quit } = require("./recipes");
+
+// TODO: Implement your CLI program here
+console.log("Welcome to the Recipe Program!\n");
+
+const recipe = createRecipe(["eggs", "flour", "sugar"]);
+console.log(recipe);
+
+const timer = setTimer(15);
+console.log(timer);
+
+const exitMessage = quit();
+console.log(exitMessage);
